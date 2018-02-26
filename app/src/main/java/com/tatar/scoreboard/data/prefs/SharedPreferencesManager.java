aac7cb72e202193675a65a6537489cd5eaa59a0f
tartarJr <mustafaogun.ozturk@gmail.com>
2018-02-26 09:41:25 +0300

    SharedPreferencesManager implemented

--- app/src/main/java/com/tatar/scoreboard/data/prefs/SharedPreferencesManager.java
+++ app/src/main/java/com/tatar/scoreboard/data/prefs/SharedPreferencesManager.java
@@ -1,8 +1,38 @@
 package com.tatar.scoreboard.data.prefs;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import com.tatar.scoreboard.data.local.modal.Hole;
+
+import java.util.List;
+
 /**
  * Created by mobile on 23.02.2018.
  */
 
 public class SharedPreferencesManager {
+
+    private static final String PREF_NAME = "com.tatar.scoreboard.preferences";
+    private static final String PREF_KEY_SCORE_COUNT = "PREF_KEY_SCORE_COUNT";
+
+    private final SharedPreferences prefs;
+
+    public SharedPreferencesManager(Context context) {
+        prefs = context.getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE);
+    }
+
+    public void setScoreCountsForHole(List<Hole> holeList) {
+        for (int i = 0; i < holeList.size(); i++) {
+            setScoreCountForHole(holeList.get(i).getScore(), i);
+        }
+    }
+
+    private void setScoreCountForHole(int scoreCount, int index) {
+        prefs.edit().putInt(PREF_KEY_SCORE_COUNT + index, scoreCount).apply();
+    }
+
+    public int getScoreCountForHole(int index) {
+        return prefs.getInt(PREF_KEY_SCORE_COUNT + index, 0);
+    }
 }
