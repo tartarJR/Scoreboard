15c71e97b51098cc110220a529f681205a24a312
tartarJr <mustafaogun.ozturk@gmail.com>
2018-02-26 15:22:37 +0300

    added context qualifiers

---
+++ app/src/main/java/com/tatar/scoreboard/di/qualifier/ApplicationContext.java
@@ -0,0 +1,15 @@
+package com.tatar.scoreboard.di.qualifier;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+import javax.inject.Qualifier;
+
+/**
+ * Created by mobile on 26.02.2018.
+ */
+
+@Qualifier
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ApplicationContext {
+}
