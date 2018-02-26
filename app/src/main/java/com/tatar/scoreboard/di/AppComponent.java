2deb19d92b7bed4ce05637b0c4c6cf346f03ce8a
tartarJr <mustafaogun.ozturk@gmail.com>
2018-02-26 11:50:27 +0300

    added AppComponent

--- app/src/main/java/com/tatar/scoreboard/di/AppComponent.java
+++ app/src/main/java/com/tatar/scoreboard/di/AppComponent.java
@@ -1,8 +1,16 @@
 package com.tatar.scoreboard.di;
 
+import com.tatar.scoreboard.di.modules.HoleProviderModule;
+import com.tatar.scoreboard.di.modules.PrefsModule;
+import com.tatar.scoreboard.ui.ScoreboardActivity;
+
+import dagger.Component;
+
 /**
  * Created by mobile on 26.02.2018.
  */
 
-public class AppComponent {
+@Component(modules = {PrefsModule.class, HoleProviderModule.class})
+public interface AppComponent {
+    void inject(ScoreboardActivity scoreboardActivity);
 }
