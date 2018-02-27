5817b819f97f0d0dad9ba858a28305e94d1b2639
tartarJr <mustafaogun.ozturk@gmail.com>
2018-02-27 09:04:03 +0300

    added top menu

--- app/src/main/java/com/tatar/scoreboard/ui/ScoreboardActivity.java
+++ app/src/main/java/com/tatar/scoreboard/ui/ScoreboardActivity.java
@@ -3,6 +3,8 @@ package com.tatar.scoreboard.ui;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.tatar.scoreboard.App;
 import com.tatar.scoreboard.R;
@@ -61,5 +63,27 @@ public class ScoreboardActivity extends AppCompatActivity {
         holeRecyclerView.setAdapter(adapter);
         holeRecyclerView.setLayoutManager(scoreboardAdapterComponent.linearLayoutManager());
         holeRecyclerView.setHasFixedSize(true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_clear_scoreboard) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
